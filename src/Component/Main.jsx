e7be11491a1c6073ce08682b92888d8001da8f15
TheKarls <carlo.bussi@outlook.it>
2021-05-28 18:54:39 +0200

    Hooks

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,55 +1,51 @@
-import React, { Component } from 'react'
-import Keys from "./keys";
+import React, { useState } from "react"
+import Keys from "./keys"
 
-export default class Main extends Component {
+export default function Main(params) {
+    const [state, setState] = useState({
+        api: Keys.API_KEY,
+        baseUrl: Keys.BASE_URL,
+        weatherimg: "",
+        name: "",
+    })
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            api: Keys.API_KEY,
-            baseUrl: Keys.BASE_URL,
-            jsonData: {},
-            weatherimg: ""
+    async function apiFetch(value) {
+        let response = await fetch(state.baseUrl + value + state.api)
+        if (!response.ok) {
+            setState((prev) => ({
+                ...prev,
+                weatherimg: "",
+                name: "",
+            }))
+            return
         }
-
-        this.apiFetch = this.apiFetch.bind(this)
+        let data = await response.json()
+        setState((prev) => ({
+            ...prev,
+            weatherimg:
+                "http://openweathermap.org/img/wn/" +
+                data.weather[0].icon +
+                "@4x.png",
+            name: data.name,
+        }))
     }
 
-
-    // http://api.openweathermap.org/data/2.5/weather?q=Asti&appid=bd6822b0071963e57429ef829170495d
-
-    async apiFetch(value) {
-        let response = await fetch(this.state.baseUrl + value + this.state.api)
-        let data = await response.json();
-        if (data.weather != null) {
-            console.log(data.weather[0].main)
-            this.setState({
-                weatherimg: "http://openweathermap.org/img/wn/"+ data.weather[0].icon +"@4x.png"
-            })
-        }
-    }
-
-    render() {
-
-        return (
-            <>
-                <div className="search-container">
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        className="search-bar"
-                        onChange={(e) => this.apiFetch(e.target.value)}
+    return (
+        <>
+            <div className="search-container">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="search-bar"
+                    onChange={(e) => apiFetch(e.target.value)}
                     //value={query}
                     // onKeyPress={search}
-                    />
-                </div>
-                <div>
-                    <img src={this.state.weatherimg} alt=""/>
-                </div>
-            </>
-
-        )
-    }
+                />
+            </div>
+            <div>
+                <img src={state.weatherimg} alt="" />
+                <h1 style={{ color: "red" }}>{state.name}</h1>
+            </div>
+        </>
+    )
 }
-
